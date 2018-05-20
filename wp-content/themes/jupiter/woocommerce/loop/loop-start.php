b058ce4406ef21a02e92b6a4be30706dbfaeeb1f
Henrique Mattos <henrique.mattos@sap.com>
2018-05-20 21:03:40 +0200

    Removing default themes and adding Jupiter

---
+++ wp-content/themes/jupiter/woocommerce/loop/loop-start.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Product Loop Start
+ *
+ * @author      WooThemes
+ * @package     WooCommerce/Templates
+ * @version     3.3.0
+ *
+ * NOTES:
+ * - 3.3.0 Introduce new class columns-{wc_get_loop_prop()} to set the number of
+ *         columns. We ignore it because the class has its own style.
+ */
+?>
+<div class="mk-product-loop compact-layout grid--float">
+	<section class="products mk--row js-el" data-grid-config='{"item":".product"}' data-mk-component="Grid">
